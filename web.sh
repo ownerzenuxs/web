dc4be3d7d60232d4c3007c8560ef1d772e113ea0
ownerzenuxs <iwantvpspls@gmail.com>
2025-01-20 11:26:08 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -7,7 +7,7 @@ function big_blue_text() {
 
 # Display "CLONING REPOSITORY" in blue and clone the GitHub repository
 big_blue_text "CLONING REPOSITORY"
-git clone https://github.com/ownerzenuxs/website.git
+git clone https://github.com/ownerzenuxs/Website.git
 
 # Change directory to the cloned TicketBot folder
 cd TicketBot || { echo "Failed to change directory to TicketBot"; exit 1; }
