354dc21d3ad144055dd77f0b3bbc056b43f0c941
ownerzenuxs <iwantvpspls@gmail.com>
2025-01-20 11:25:00 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# Function to display big blue text
+function big_blue_text() {
+    echo -e "\e[1;34m$1\e[0m"
+}
+
+# Display "CLONING REPOSITORY" in blue and clone the GitHub repository
+big_blue_text "CLONING REPOSITORY"
+git clone https://github.com/ownerzenuxs/website.git
+
+# Change directory to the cloned TicketBot folder
+cd TicketBot || { echo "Failed to change directory to TicketBot"; exit 1; }
+
+# Display "INSTALLING NPM DEPENDENCIES" in blue and install discord.js
+big_blue_text "INSTALLING NPM DEPENDENCIES"
+npm install express pm2 || { echo "Failed to install discord.js"; exit 1; }
+
+# Display completion message
+big_blue_text "DEPENDENCIES INSTALLED. READY TO RUN YOUR BOT!"
